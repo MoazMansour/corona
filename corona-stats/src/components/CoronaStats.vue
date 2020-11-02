470084cf9eaee46c1228040c885ca398991c4cdf
Syed Gilani <sgilani@dmdtemp2020.local>
2020-11-02 16:23:08 -0500

    front end changes

--- corona-stats/src/components/CoronaStats.vue
+++ corona-stats/src/components/CoronaStats.vue
@@ -1,58 +1,132 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="mx-9">
+    <v-data-table
+      dense
+      fixed-header
+      v-model="selected"
+      item-key="dataset"
+      loading-text="Loading US Corona Virus Statistics..."
+      :headers="headers"
+      :items="results"
+      :options.sync="options"
+      :loading="loading"
+      :items-per-page="defaultPageSize"
+      show-select
+      multi-sort
+      class="mt-2 elevation-5"
+      :footer-props="footerProps"
+      >
+    <template v-slot:top="{ pagination, options, updateOptions }">
+        <v-data-footer
+          :pagination="pagination"
+          :options="options"
+          @update:options="updateOptions"
+          firstIcon="mdi-arrow-collapse-left"
+          lastIcon="mdi-arrow-collapse-right"
+          prevIcon="mdi-chevron-left"
+          nextIcon="mdi-chevron-right"
+          :items-per-page-options="pageSizeOptions"
+          items-per-page-text="$vuetify.dataTable.itemsPerPageText"/>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  import axios from 'axios'
+  import moment from 'moment'
+  export default {
+    name: 'CoronaStats',
+    props: {
+      pageSizeOptions: {
+        type: Array,
+        default: () => [50, 100, 200, 500, 1000, 5000]
+      },
+      defaultPageSize: {
+        type: Number,
+        default: 500
+      }
+    },
+    data () {
+      return {
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+        /* Grid stuff */
+        selected: [],
+        totalResults: 0,
+        results: [],
+        loading: true,
+        options: {},
+        footerProps: {
+          firstIcon: 'mdi-arrow-collapse-left',
+          lastIcon: 'mdi-arrow-collapse-right',
+          prevIcon: 'mdi-chevron-left',
+          nextIcon: 'mdi-chevron-right',
+          'items-per-page-options': this.pageSizeOptions
+        },
+        headers: [
+          {
+            text: 'Dataset',
+            align: 'start',
+            sortable: true,
+            value: 'data_set_name'
+          },
+          { text: 'State Name', value: 'state' },
+          { text: 'Updated', value: 'updated' },
+          { text: 'Total Cases', value: 'cases' },
+          { text: 'Cases Today', value: 'casesToday' },
+          { text: 'Total Deaths', value: 'deaths' },
+          { text: 'Deaths Today', value: 'deathsToday' },
+          { text: 'Recovered Cases', value: 'recovered' },
+          { text: 'Active Cases', value: 'active' },
+          { text: 'Cases Per Million', value: 'Cases Per Million' },
+          { text: 'Deaths Per Million', value: 'deathsPerMillion' },
+          { text: 'Total Tests', value: 'tests' },
+          { text: 'Tests Per Million', value: 'testsPerMillion' },
+          { text: 'Total Population', value: 'pop' },
+        ]
+      }
+    },
+    mounted () {
+      this.getDataFromApi()
+        .then(data => {
+          this.results = data.items
+          this.totalResults = data.total
+        })
+    },
+    methods: {
+      formatDate (value) {
+        return moment(value).format('YYYY-MM-DD HH:mm:ss')
+      },
+      getDataFromApi () {
+        this.loading = true
+        return new Promise((resolve) => {
+
+        let items
+        let total
+
+        axios
+          .get('http://127.0.0.1:8000/all_states', {
+            params: {
+            }
+          })
+          .then(response => {
+            total = response.data.length
+            items = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+          .finally(() => {
+            this.loading = false
+            resolve({
+              items,
+              total
+            })
+          })
+        })
+      }
+    }
+  }
+
+
+</script>
--- main.py
+++ main.py
@@ -4,8 +4,24 @@ from models import StateSummary
 from get_data import get_data
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
